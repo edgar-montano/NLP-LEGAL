2490731491a9a874b1042ecd09d790271bcc3b27
Edgar Montano <em2532@nyu.edu>
2016-05-09 14:31:00 -0400

    added applause detection system

--- applause.py
+++ applause.py
@@ -4,14 +4,24 @@ import math
 import nltk
 import re
 
+
 #looks for applause to denote for key words
 #test data
 #research paper
 
 def main():
     if(len(sys.argv)==1):
-        print("[USAGE]: python applause.py <filename>")
+        print("[USAGE]: python applause.py <filename> (buffsize)")
+        print("\t note buffsize is optional")
         print("[Example]: python applause.py trascripts/trump/1-24-06...iowa_freed_speech.txt")
+
+    if(sys.argv[2]):
+        buffmax = int(sys.argv[2])
+    else:
+        buffmax = 50
+
+    currbuff = 0
+    buff = []
 
     with open(sys.argv[1],'r') as fn:
 
@@ -20,10 +30,14 @@ def main():
 
             words = nltk.word_tokenize(line.decode('utf-8').lower())
             for word in words:
+                if(currbuff != buffmax):
+                    buff.append(word)
+
                 #print("word="+word)
             ##print(word+"==")
                 if(word == 'laughter' or word == 'applause'  or word == 'cheers'):
-                    print("phrase="+word)
+                    print(buffer)
+                    #print("phrase="+word)
 
         #print(lines[0].split(" "))
         # count = 0
