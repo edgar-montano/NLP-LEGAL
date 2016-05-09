f6a8306994edce1bd43676a855c4623f705d2829
Edgar Montano <em2532@nyu.edu>
2016-05-09 14:48:44 -0400

    now can create small instances with buff size and buff max

--- applause.py
+++ applause.py
@@ -3,6 +3,7 @@ import os
 import math
 import nltk
 import re
+import pickle
 
 
 #looks for applause to denote for key words
@@ -15,13 +16,14 @@ def main():
         print("\t note buffsize is optional")
         print("[Example]: python applause.py trascripts/trump/1-24-06...iowa_freed_speech.txt")
 
-    if(sys.argv[2]):
+    if(len(sys.argv)==3):
         buffmax = int(sys.argv[2])
     else:
-        buffmax = 50
+        buffmax = 100
 
     currbuff = 0
     buff = []
+    picke_file_iteration = 0 #determines extension of  pickle file
 
     with open(sys.argv[1],'r') as fn:
 
@@ -30,13 +32,23 @@ def main():
 
             words = nltk.word_tokenize(line.decode('utf-8').lower())
             for word in words:
-                if(currbuff != buffmax):
-                    buff.append(word)
+                #reset the buffer
+                if(currbuff == buffmax):
+                    currbuff = 0
+                    buff = []
+
+                buff.append(word)
+                currbuff += 1
 
                 #print("word="+word)
             ##print(word+"==")
                 if(word == 'laughter' or word == 'applause'  or word == 'cheers'):
-                    print(buffer)
+                    fn = sys.argv[1]+str(picke_file_iteration)+".pickle"
+                    picke_file_iteration += 1
+                    print("printing buffer\n--------------\n")
+                    with open(fn, 'wb') as handle:
+                        pickle.dump(buff, handle)
+                    print(buff)
                     #print("phrase="+word)
 
         #print(lines[0].split(" "))
